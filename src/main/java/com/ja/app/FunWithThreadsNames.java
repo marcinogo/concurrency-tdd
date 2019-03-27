104fe49e01e519567304a4c5ca4182d4cb2aadb8
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2019-03-27 21:29:51 +0100

    Complete task 3

--- src/main/java/com/ja/app/FunWithThreadsNames.java
+++ src/main/java/com/ja/app/FunWithThreadsNames.java
@@ -5,9 +5,9 @@ package com.ja.app;
  */
 public class FunWithThreadsNames {
     public static void main(String[] args) {
-        new MyThread("ThreadExtend").start();
-        new Thread(new MyRunnable("RunnableImplement")).start();
-        new Thread(new MyRunnable("Other RunnableImpl")).start();
+        new MyThread("ALA").start();
+        new Thread(new MyRunnable("MA", 100)).start();
+        new Thread(new MyRunnable("KOTA")).start();
     }
 
     static class MyThread extends Thread {
@@ -17,19 +17,35 @@ public class FunWithThreadsNames {
 
         @Override
         public void run() {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
             System.out.println(this.getName());
         }
     }
 
     static class MyRunnable implements Runnable {
         private String name;
+        private int sleepTime;
 
         public MyRunnable(String name) {
             this.name = name;
         }
 
+        public MyRunnable(String name, int sleepTime) {
+            this.name = name;
+            this.sleepTime = sleepTime;
+        }
+
         @Override
         public void run() {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
             System.out.println(name);
         }
     }
